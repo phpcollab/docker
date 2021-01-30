3ba01357946f38851f7806fed054865d03515796
John Gasper <jtgasper3@gmail.com>
2021-01-30 12:13:09 -0800

    Basic entrypoint and startup and CI working (#4)
    Co-authored-by: Jeff Sittler <mindblender@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-phpCollab Docker Image
-======================
+# phpCollab Docker Image
 
 This repository host the source to product a Docker Image for [phpCollab](https://phpcollab.com/?utm_source=dockerhub&utm_medium=cpc&utm_campaign=docker_hub).
+
+## ... via [`docker stack deploy`](https://docs.docker.com/engine/reference/commandline/stack_deploy/) or [`docker-compose`](https://github.com/docker/compose)
+
+Example `stack.yml` for `phpcollab`:
+
+```yaml
+version: "3.1"
+
+services:
+  phpcollab:
+    image: phpcollab/phpcollab
+    restart: always
+    ports:
+      - 8080:80
+    environment:
+      PHPCOLLAB_DB_HOST: db
+      PHPCOLLAB_DB_USER: exampleuser
+      PHPCOLLAB_DB_PASSWORD: examplepass
+      PHPCOLLAB_DB_NAME: exampledb
+    volumes:
+      - phpcollab:/var/www/phpcollab
+
+  db:
+    image: mysql:5.7
+    restart: always
+    environment:
+      MYSQL_DATABASE: exampledb
+      MYSQL_USER: exampleuser
+      MYSQL_PASSWORD: examplepass
+      MYSQL_RANDOM_ROOT_PASSWORD: "1"
+    volumes:
+      - db:/var/lib/mysql
+
+volumes:
+  phpcollab:
+  db:
+```
---
+++ latest/php7.3/fpm-alpine/docker-entrypoint.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+set -euo pipefail
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
+	if [ "$(id -u)" = '0' ]; then
+		case "$1" in
+			apache2*)
+				user="${APACHE_RUN_USER:-www-data}"
+				group="${APACHE_RUN_GROUP:-www-data}"
+
+				# strip off any '#' symbol ('#1000' is valid syntax for Apache)
+				pound='#'
+				user="${user#$pound}"
+				group="${group#$pound}"
+				;;
+			*) # php-fpm
+				user='www-data'
+				group='www-data'
+				;;
+		esac
+	else
+		user="$(id -u)"
+		group="$(id -g)"
+	fi
+
+	envs=(
+		PHPCOLLAB_DB_HOST
+		PHPCOLLAB_DB_USER
+		PHPCOLLAB_DB_PASSWORD
+		PHPCOLLAB_DB_NAME
+		PHPCOLLAB_DB_TYPE
+	)
+	haveConfig=
+	for e in "${envs[@]}"; do
+		file_env "$e"
+		if [ -z "$haveConfig" ] && [ -n "${!e}" ]; then
+			haveConfig=1
+		fi
+	done
+
+	# only touch "includes/settings.php" if we have environment-supplied configuration values
+	if [ "$haveConfig" ]; then
+		while ! curl -s ${PHPCOLLAB_DB_HOST}:3306 > /dev/null; do
+			echo waiting for mysql to start
+			sleep 3;
+		done
+		sleep 3;
+
+		if ! TERM=dumb php -- <<'EOPHP'
+<?php
+use phpCollab\Installation\Installation;
+
+error_reporting(2039);
+
+define('APP_ROOT', dirname(__FILE__));
+
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+try {
+    $settingsData = array(
+        "dbServer" => getenv('PHPCOLLAB_DB_HOST'),
+        "dbLogin" => getenv('PHPCOLLAB_DB_USER'),
+        "dbPassword" => getenv('PHPCOLLAB_DB_PASSWORD'),
+        "dbName" => getenv('PHPCOLLAB_DB_NAME'),
+        "databaseType" => getenv('PHPCOLLAB_DB_TYPE'),
+        "siteUrl" => getenv('PHPCOLLAB_SITE_URL'),
+        "adminEmail" => getenv('PHPCOLLAB_ADMIN_EMAIL'),
+        "adminPassword" => bin2hex(openssl_random_pseudo_bytes(5)),
+		"loginMethod" => "crypt",
+        "appRoot" => APP_ROOT
+    );
+
+    $installation = new Installation([
+        'dbServer' => $settingsData["dbServer"],
+        'dbUsername' => getenv('PHPCOLLAB_DB_USER'),
+        'dbPassword' => $settingsData["dbPassword"],
+        'dbName' => $settingsData["dbName"],
+        'dbType' => $settingsData["databaseType"],
+    ], $settingsData["appRoot"]);
+
+    echo <<<SETUP_INTRO
+Installing \e[1;34mphpCollab\e[0m...
+
+SETUP_INTRO;
+
+
+    if ($installation->setup($settingsData)) {
+        // If setup returns true, then output the password to the CLI
+        echo <<<ADMIN_PW_OUTPUT
+\e[0;32m==================
+| ADMIN PASSWORD |
+==================\e[0m
+🔒 The admin password has been set to: {$settingsData["adminPassword"]}
+==================
+
+ADMIN_PW_OUTPUT;
+    };
+} catch (PDOException $e) {
+    echo <<<EXCEPTION
+\e[0;31m==================
+❗️ ERROR: Database
+==================
+{$e}\e[0m
+
+EXCEPTION;
+    return false;
+} catch (Exception $e) {
+    echo <<<EXCEPTION
+\e[0;31m==================
+❗️ ERROR: Setup
+==================
+{$e}\e[0m
+
+EXCEPTION;
+    return false;
+}
+?>
+EOPHP
+			rm -rf installation/
+		then
+			echo >&2
+			echo >&2 "WARNING: unable to establish a database connection to '$PHPCOLLAB_DB_HOST'"
+			echo >&2 '  continuing anyways (which might have unexpected results)'
+			echo >&2
+		fi
+	fi
+
+	# now that we're definitely done writing configuration, let's clear out the relevant envrionment variables (so that stray "phpinfo()" calls don't leak secrets from our code)
+	for e in "${envs[@]}"; do
+		unset "$e"
+	done
+fi
+
+exec "$@"
